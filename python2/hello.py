a2378ba0d0d689fbbd6a456d62f44f4dec200c71
akiaki369 <cbye1210369@gn.iwasaki.ac.jp>
2022-05-11 14:38:23 +0900

    render template 2nd

--- python2/hello.py
+++ python2/hello.py
@@ -6,9 +6,9 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('hello.html')
+@app.route("/<name>")
+def hello_world(name):
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run()
