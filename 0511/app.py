742ec86bc8a0f0993fb283aa9eed8389f738e6ad
akiaki369 <cbye1210369@gn.iwasaki.ac.jp>
2022-05-11 20:42:07 +0900

    updated

--- 0511/app.py
+++ 0511/app.py
@@ -9,9 +9,18 @@ def index():
 @app.route('/result', methods=["GET"])
 def result_get():
     field = int(request.args.get("field", ""))
+    if field % 2 == 0:
+        result="偶数"
+    else:
+        result="奇数"
+    return render_template('result.html', message="{}は{}です。".format(field, result))
+
+@app.route('/result', methods=["POST"])
+def result_post():
+    field = int(request.form["field"])
 
     for k in range(2, field):
-        if field % 2 == 0:
+        if field % k == 0:
             result = "素数ではありません"
             break
         else:
